2af2339e4c5f8b786945d265936a141568b8818f
noamtz <noam185@gmail.com>
2016-03-13 12:39:14 +0200

    Add debug logs

--- node-gcm-ccs.js
+++ node-gcm-ccs.js
@@ -24,6 +24,7 @@ module.exports = function GCMClient(projectId, apiKey) {
 
 	function _send(json) {
 		if (draining) {
+			console.log("[node-gcm-ccs][_send] draining is on! queueing json...:", JSON.parse(json));
 			queued.push(json);
 		} else {
 			var message = new xmpp.Stanza.Element('message').c('gcm', { xmlns: 'google:mobile:data' }).t(JSON.stringify(json));
@@ -32,11 +33,13 @@ module.exports = function GCMClient(projectId, apiKey) {
 	}
 
 	client.on('online', function() {
+		console.log("[node-gcm-ccs][client][online] GCM server is online");
 		events.emit('connected');
 
 		if (draining) {
 			draining = false;
 			var i = queued.length;
+			console.log("[node-gcm-ccs][client][online] need to draining. queue size:", i, "queue:", JSON.stringify(queued));
 			while (i--) {
 				_send(queued[i]);
 			}
@@ -45,6 +48,7 @@ module.exports = function GCMClient(projectId, apiKey) {
 	});
 
 	client.on('close', function() {
+		console.log("[node-gcm-ccs][client][close] draining:", draining);
 		if (draining) {
 			client.connect();
 		} else {
@@ -60,18 +64,22 @@ module.exports = function GCMClient(projectId, apiKey) {
 		if (stanza.is('message') && stanza.attrs.type !== 'error') {
 			var data = JSON.parse(stanza.getChildText('gcm'));
 
+			console.log("[node-gcm-ccs][client][stanza] message is coming: ");
 			if (!data || !data.message_id) {
+				console.log("[node-gcm-ccs][client][stanza] wrong data: ", data);
 				return;
 			}
 
 			switch (data.message_type) {
 				case 'control':
+					console.log("[node-gcm-ccs][client][stanza][control] wrong data: ", JSON.stringify(data));
 					if (data.control_type === 'CONNECTION_DRAINING') {
 						draining = true;
 					}
 					break;
 
 				case 'nack':
+					console.log("[node-gcm-ccs][client][stanza][nack] iterate over acks: ", JSON.stringify(acks));
 					if (data.message_id in acks) {
 						acks[data.message_id](data.error);
 						delete acks[data.message_id];
@@ -79,6 +87,7 @@ module.exports = function GCMClient(projectId, apiKey) {
 					break;
 
 				case 'ack':
+					console.log("[node-gcm-ccs][client][stanza][ack] iterate over acks: ", JSON.stringify(acks));
 					if (data.message_id in acks) {
 						acks[data.message_id](undefined, data.message_id, data.from);
 						delete acks[data.message_id];
@@ -86,10 +95,12 @@ module.exports = function GCMClient(projectId, apiKey) {
 					break;
 
 				case 'receipt':
+					console.log("[node-gcm-ccs][client][stanza][receipt] messageId:", data.message_id);
 					events.emit('receipt', data.message_id, data.from, data.category, data.data);
 					break;
 
 				default:
+					console.log("[node-gcm-ccs][client][stanza][default] data:", JSON.stringify(data));
 					// Send ack, as per spec
 					if (data.from) {
 						_send({
@@ -107,6 +118,7 @@ module.exports = function GCMClient(projectId, apiKey) {
 			}
 		} else {
 			var message = stanza.getChildText('error').getChildText('text');
+			console.log("[node-gcm-ccs][client][stanza] error is coming: ", JSON.stringify(message));
 			events.emit('message-error', message);
 		}
 	});
@@ -131,12 +143,15 @@ module.exports = function GCMClient(projectId, apiKey) {
 	}
 
 	function end() {
+		console.log("[node-gcm-ccs][client][end] ending client.. ");
 		client.end();
 	}
-	function isReady(){
 
-	 return Object.keys(acks).length <=100;
+	function isReady() {
+		console.log("[node-gcm-ccs][isReady] Object.keys(acks).length: ", Object.keys(acks).length, "isReady", Object.keys(acks).length <= 100);
+		return Object.keys(acks).length <=100;
 	}
+
 	events.end = end;
 	events.send = send;
 	events.isReady= isReady;
